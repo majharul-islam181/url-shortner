693dc67c055b1e3a9e29fb4281956a9520c1bae8
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-04-11 20:34:40 +0600

    url sturct

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
 
 func main() {
 	fmt.Println("Creating Url-Shortner")
